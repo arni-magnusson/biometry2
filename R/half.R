1f7d043023692504e9af10f5a76382975807e158
RagnhildurG <rag41@his>
2019-02-14 14:49:40 +0000

    Add new function  "half".

---
+++ R/half.R
@@ -0,0 +1,26 @@
+#' Divide by Two
+#'
+#' This function divides x by 2.
+#'
+#' @param x a number.
+#'
+#' @return
+#' A number, half the value of x.
+#'
+#' @author
+#' Ragnhildur Gudmundsdottir
+#'
+#' @seealso 
+#' \link{Arithmetic}, \code{\link{twice}}.
+#' 
+#' @examples
+#' half(5)
+#'  
+#' half(pi)
+#'  
+#' @export  
+    
+half <- function(x)
+{
+  x/2
+}
