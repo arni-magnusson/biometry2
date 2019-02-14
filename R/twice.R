3bfec22da94ffc27c793f08db6744f64639a6361
Arni Magnusson <arnima@hafro.is>
2019-02-14 11:19:25 +0000

    Roxygenize help page for twice()

--- R/twice.R
+++ R/twice.R
@@ -1,3 +1,25 @@
+#' Multiply by Two
+#'
+#' This function multiplies x by 2.
+#'
+#' @param x a number.
+#'
+#' @return
+#' A number, two times the value of x.
+#'
+#' @author
+#' Arni Magnusson
+#'
+#' @seealso
+#' \link{Arithmetic}
+#'
+#' @examples
+#' twice(10)
+#'
+#' twice(pi)
+#'
+#' @export
+
 twice <- function(x)
 {
   2*x
